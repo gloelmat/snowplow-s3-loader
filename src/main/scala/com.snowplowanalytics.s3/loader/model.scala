43736a929a7906ca3a17c1683b96e8512a77d22e
Matthias Gloel <gloelmat@mar-mbpro-08.dmc.local>
2020-05-17 11:14:17 +0200

    Add dynamoDB customendpoint for localstack

--- src/main/scala/com.snowplowanalytics.s3/loader/model.scala
+++ src/main/scala/com.snowplowanalytics.s3/loader/model.scala
@@ -39,7 +39,8 @@ package model {
     maxRecords: Long,
     region: String,
     appName: String,
-    customEndpoint: Option[String]
+    customEndpoint: Option[String],
+    dynamodbCustomEndpoint: Option[String]
   ) {
     val timestampEither = initialTimestamp
       .toRight("An initial timestamp needs to be provided when choosing AT_TIMESTAMP")
@@ -54,6 +55,11 @@ package model {
       case "cn-north-1" => "kinesis.cn-north-1.amazonaws.com.cn"
       case _ => s"https://kinesis.$region.amazonaws.com"
     })
+
+    val dynamodbEndpoint = dynamodbCustomEndpoint.getOrElse(region match {
+        case "cn-north-1" => s"https://dynamodb.cn-north-1.amazonaws.com.cn"
+        case _ => s"https://dynamodb.$region.amazonaws.com"
+      })
   }
   case class BufferConfig(byteLimit: Long, recordLimit: Long, timeLimit: Long)
   case class StreamsConfig(
